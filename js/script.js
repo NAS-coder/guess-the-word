8f583b2841add861bbb5415048b25b01baec799c
Nabeel <nabeel.siddiqui@rocketmail.com>
2023-01-12 11:16:23 +0400

    new rules added to the game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLetter = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const textInput = document.querySelector(".letter");
 const wip = document.querySelector(".word-in-progress");
@@ -7,6 +7,7 @@ const remainingGuesses = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const hiddenButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
 const start = function (word) {
     selectedLetters = [];
@@ -22,9 +23,38 @@ start(word);
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     const inputCapture = textInput.value;
-    console.log(inputCapture);
+    //console.log(inputCapture);
     //for (const e of textInput) {
     //    inputCapture.splice();
  // };
  textInput.value = "";
+ message.innerText = "";
+ const pressingButton = validation(inputCapture)
+ makeGuess(inputCapture);
 })
+
+
+
+const validation = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText =  "Please, enter a letter."
+    } else if (input.length > 1 ) {
+        message.innerText = "Error. Please, enter only one letter at a time."
+    } else if (! input.match(acceptedLetter)) {
+        message.innerText = "Please, enter a letter from A to Z."
+    } else {
+        return input;
+   }
+}
+
+const makeGuess = function (letter) {
+    letter = letter.toUpperCase();
+    if (guessedLetters.includes(letter)) {
+        message.innerText = "Letter already guessed. Choose another letter."
+    } else {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+    }
+   
+}
